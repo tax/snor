dab837dfbcf0b2036e40689693165be264875f7f
Paul <paultax@gmail.com>
2013-05-29 21:35:29 +0200

    Add simple utorrent client

--- snor/clients/transmission.py
+++ snor/clients/transmission.py
@@ -5,25 +5,25 @@ from snor.clients import BaseCLient
 
 
 class Client(BaseCLient):
-	_name = 'transmission'
-	_settings = {
-		'port': 9091,
-		'address':'localhost', 
-		'user':None,
-		'password':None
-	}
-	
-	def add_magnet_hash(self, magnet_hash, download_dir):
-		c = transmissionrpc.Client(**self.get_settings())
-		url = 'magnet:?xt=urn:btih:{h}'.format(h=magnet_hash)
-		return c.add_torrent(url, download_dir=download_dir)
+    _name = 'transmission'
+    _settings = {
+        'port': 9091,
+        'address':'localhost', 
+        'user':None,
+        'password':None
+    }
+    
+    def add_magnet_hash(self, magnet_hash, download_dir):
+        c = transmissionrpc.Client(**self.get_settings())
+        url = 'magnet:?xt=urn:btih:{h}'.format(h=magnet_hash)
+        return c.add_torrent(url, download_dir=download_dir)
 
-	def get_completed(self):
-		print 'Completed'
+    def get_completed(self):
+        print 'Completed'
 
-	def is_active(self):
-		try:
-			transmissionrpc.Client(**self.get_settings())
-		except:
-			return False
-		return True
+    def is_active(self):
+        try:
+            transmissionrpc.Client(**self.get_settings())
+        except:
+            return False
+        return True
--- snor/clients/utorrent.py
+++ snor/clients/utorrent.py
@@ -1,14 +1,39 @@
-import transmissionrpc
+import requests
 from snor.clients import BaseCLient
 
 class Client(BaseCLient):
+    _name = 'utorrent'
+    _settings = {
+        'port': 9091,
+        'address':'localhost', 
+        'user':None,
+        'password':None
+    }
 
-	def add_magnet_link(self, url, dowload_dir):
-		client = transmissionrpc.Client('localhost', port=9091)
-		return client.add_torrent(url, download_dir=download_dir)
+    def add_magnet_link(self, url, dowload_dir):
+        magnet_url = url
+        auth = (self._settings['user'],self._settings['password'])
+        url = 'http://{address}:{port}/gui/'.format(**self._settings)
+        r = requests.get(url + 'token.html', auth=auth)
+        cookie = r.cookies
+        # Extract token
+        token_re = "<div id='token' style='display:none;'>([^<>]+)</div>"
+        match = re.search(token_re, r.content)
+        token = match.group(1)
 
-	def get_completed(self):
-		print 'Completed'
+        #Set to correct download folder
+        d = download_dir
+        p = {'token':token, 'action':'setsetting', 's':'dir_completed_download', 'v':d}
+        r = requests.get(url, params=p, auth=auth, cookies=cookie)
+        print r.status_code
+        p = {'token':token, 'action':'setsetting', 's':'dir_completed_download_flag', 'v':'1'}
+        requests.get(url, params=p, auth=auth, cookies=cookie)
+        # Add magnet hash
+        p = {'token':token, 'action':'add-url', 's':magnet_url}
+        requests.get(url, params=p, auth=auth, cookies=cookie)
 
-	def is_active(self):
-		return True
+    def get_completed(self):
+        print 'Completed'
+
+    def is_active(self):
+        return True
